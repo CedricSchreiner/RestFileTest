b98eeed9200c1da934a43a0689b904f6504d57cb
weedric <cedi.schreiner@web.de>
2018-01-30 23:10:29 +0100

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,54 @@
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Main().start();
+    }
+
+    private Main() {}
+
+    private void start() {
+        File file = new File("C:\\Users\\Cedric\\Desktop\\testfilme.txt");
+        String BASE_URL="http://192.168.178.25:8080/api/files/upload";
+        HttpClient client = HttpClientBuilder.create().build();
+        HttpPost postRequest = new HttpPost(BASE_URL) ;
+        try
+        {
+
+            //Set various attributes
+            //MultipartEntity multiPartEntity = new MultipartEntity () ;
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+
+            FileBody fileBody = new FileBody(file, "application/octect-stream") ;
+            //Prepare payload
+            builder.addPart("attachment", fileBody);
+            //multiPartEntity.addPart("attachment", fileBody) ;
+
+            //Set to request body
+            //postRequest.setEntity(multiPartEntity) ;
+            postRequest.setEntity(builder.build());
+
+            //Send request
+            HttpResponse response = client.execute(postRequest) ;
+
+            //Verify response if any
+            if (response != null)
+            {
+                System.out.println(response.getStatusLine().getStatusCode());
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace() ;
+        }
+    }
+}
